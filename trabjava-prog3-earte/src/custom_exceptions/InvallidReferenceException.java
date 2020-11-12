f6783d045b7d46cd53a43157e4294e0fb77e90c0
Lorenzo1459 <44271727+Lorenzo1459@users.noreply.github.com>
2020-11-12 19:11:26 -0300

    Delete InvallidReferenceException.java

--- trabjava-prog3-earte/src/custom_exceptions/InvallidReferenceException.java
+++
@@ -1,7 +0,0 @@
-package custom_exceptions;
-
-public class InvallidReferenceException extends RuntimeException{
-	public InvallidReferenceException(String msgErro, Throwable e) {
-		super(msgErro, e);
-	}
-}
