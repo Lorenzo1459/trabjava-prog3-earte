2c9391ed87b2f3944bc4760ad8205fdc1020d067
Lorenzo1459 <44271727+Lorenzo1459@users.noreply.github.com>
2020-11-12 19:11:31 -0300

    Delete RepeatedRegisterException.java

--- trabjava-prog3-earte/src/custom_exceptions/RepeatedRegisterException.java
+++
@@ -1,7 +0,0 @@
-package custom_exceptions;
-
-public class RepeatedRegisterException extends RuntimeException{
-	public RepeatedRegisterException(String msgErro, Throwable e) {
-		super(msgErro, e);
-	}
-}
